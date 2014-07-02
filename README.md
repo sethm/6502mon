21bf94ba8b6a5bd83d72f2494827e0dfdec23459
Seth Morabito <seth@loomcom.com>
2014-07-01 18:13:21 -0700

    Initial Checkin

---
+++ README.md
@@ -0,0 +1,21 @@
+# Loomcom 6502 ROM Monitor
+
+**Version:** 0.1-alpha  
+**Last Updated:** July 1, 2014
+
+## 1.0 About
+
+This project contains an experimental 6502 ROM monitor being developed
+for the [Symon 6502 Simulator] (http://github.com/sethm/symon) and
+associated hardware.
+
+## 2.0 Assembly
+
+Assembled with [CA65] (http://www.cc65.org/doc/ca65.html). Just
+type `make`!
+
+## 3.0 License
+
+This project is free software. It is distributed under the MIT
+License. Please see the file 'COPYING' for full details of the
+license.
---
+++ monitor.asm
@@ -0,0 +1,31 @@
+;;
+;; Retrochallenge Summer 2014
+;; 6502 ROM Monitor
+;;
+
+
+;;************************************************************************
+;; Non-monitor code, e.g. BASIC, utilities, etc., resides
+;; in the bottom 14KB of ROM
+;;************************************************************************
+.segment	"CODE"
+		.org 	$C000
+
+;;************************************************************************
+;; ROM monitor code resides in the top 2KB of ROM
+;;************************************************************************
+.segment	"MONITOR"
+		.org	$FB00
+
+START:		LDA	#$00	; Set zero flag
+		BEQ	*	; Loop forever
+
+;;************************************************************************
+;;
+;;************************************************************************
+.segment	"VECTORS"
+		.org	$FFFA
+
+		.word	START
+		.word	START
+		.word	START
